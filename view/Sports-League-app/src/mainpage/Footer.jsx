7a1356870bb236e62f6a55eb9d2e2f93bbee50c2
jonathanDargakis <jmdargakis@loyola.edu>
2025-10-23 15:09:12 -0400

    pages folder and Footer #25

---
+++ view/Sports-League-app/src/mainpage/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <div class="mt-5 p-4 bg-dark text-white text-center">
+            <p>Youth Sports League</p>
+        </div>
+    );
+}
