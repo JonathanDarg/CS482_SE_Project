2891a64bc500aba8caf422e60f9716efb56e6769
jonathanDargakis <jmdargakis@loyola.edu>
2025-10-24 12:05:48 -0400

    Game to Event name and EventController #22

--- controller/controller.md
+++
@@ -1,2 +0,0 @@
-# Controller Module
-Handles incoming requests, processes data via the Model layer, and returns responses (e.g., Express route handlers).
---
+++ model/EventDao.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+    location: String,
+    dateTime: {type: Date, default: Date.now},
+    rating: Number,
+    typeOfMatch: String
+});
+
+const EventModel = mongoose.model('Event', EventSchema);
+
+exports.createEvent = async function(EventData){
+    let Event = new EventModel(EventData);
+    await Event.save();
+    return Event;
+}
+
+exports.getAllEvents = async function(){
+    let lstEvents = await EventModel.find();
+    return lstEvents;
+}
+
+exports.readOneEvent = async function(id){
+    let Event = await EventModel.findById(id);
+    return Event;
+}
+
+exports.getByMonth = async function(month, year){
+    let startDate = new Date(year, month - 1, 1);
+    let endDate = new Date(year, month, 1);
+
+    let Events = await EventModel.find({
+        dateTime: {
+            $gte: startDate,
+            $lt: endDate
+        }
+    });
+    
+    return Events;
+}
+
+exports.updateEvent = async function(id, EventData){
+    let Event = await EventModel.findById(id);
+    if(!Event) return null;
+    Event.location = EventData.location;
+    Event.dateTime = EventData.dateTime;
+    Event.rating = EventData.rating;
+    Event.typeOfMatch = EventData.typeOfMatch;
+    await Event.save();
+    return Event;
+}
+
+exports.deleteEvent = async function(id){
+    await EventModel.findByIdAndDelete(id);
+}
+
+exports.deleteAll = async function name(){
+    await EventModel.deleteMany();
+}
+
